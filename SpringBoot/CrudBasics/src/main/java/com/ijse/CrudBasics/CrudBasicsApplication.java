ab9b165a64e28dcaa38304d0e0b227af96201f99
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-31 17:49:35 +0530

    create project for study basic crud project and setup db connection

---
+++ SpringBoot/CrudBasics/src/main/java/com/ijse/CrudBasics/CrudBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.ijse.CrudBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBasicsApplication.class, args);
+	}
+
+}
