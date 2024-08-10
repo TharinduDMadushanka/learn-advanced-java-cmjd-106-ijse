81ec30cf408be7cae65c1d750217ef2f64e91402
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-10 10:40:36 +0530

    add getProductById and implemenet it

--- SpringBoot/many-to-many/springintro/src/main/java/com/ijse/springintro/controller/OrderController.java
+++ SpringBoot/many-to-many/springintro/src/main/java/com/ijse/springintro/controller/OrderController.java
@@ -1,11 +1,15 @@
 package com.ijse.springintro.controller;
 
+import com.ijse.springintro.dto.OrderDto;
 import com.ijse.springintro.entity.Order;
+import com.ijse.springintro.entity.Product;
 import com.ijse.springintro.service.OrderService;
+import com.ijse.springintro.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,6 +20,9 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/orders")
     public ResponseEntity<List<Order>> getAllOrders() {
         List<Order> orders = orderService.getAllOrders();
@@ -23,6 +30,17 @@ public class OrderController {
     }
 
     @PostMapping("/orders")
+    public ResponseEntity<Order> createOrder(@RequestBody OrderDto orderDto) {
+        Order order = new Order();
 
+        // get product ids from order dtos to productIds array
+        List<Long> productIds = orderDto.getOrderIds();
+
+        productIds.forEach(productId ->{
+            //get product by id
+//            Product product = productService.getProductById();
+            // add this product to order
+        });
+    }
 
 }
