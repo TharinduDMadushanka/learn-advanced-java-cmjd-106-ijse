df6508353f6a9ffafc5779619040392681eec96f
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-10 10:03:05 +0530

    creating OrderServiceImpl & implements OrderService and impletement methods

---
+++ SpringBoot/many-to-many/springintro/src/main/java/com/ijse/springintro/service/OrderServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ijse.springintro.service;
+
+import com.ijse.springintro.entity.Order;
+import com.ijse.springintro.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService{
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Override
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    @Override
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
+}
