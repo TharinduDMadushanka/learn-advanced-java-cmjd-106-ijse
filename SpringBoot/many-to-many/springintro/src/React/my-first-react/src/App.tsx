f2b845c4c3d0e95e7b38d17b51ba2a97ce235c4e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-17 09:58:47 +0530

    move App function to Home.tsx

--- SpringBoot/many-to-many/springintro/src/React/my-first-react/src/App.tsx
+++ SpringBoot/many-to-many/springintro/src/React/my-first-react/src/App.tsx
@@ -1,41 +1,11 @@
 import './App.css';
-import Vehicle from "./components/vehicle.tsx";
-import {useState} from "react";
+
+// @ts-ignore
+import Home from "./pages/Home.tsx";
 
 // Parent component
 function App() {
-    // useState hook for username
-    const [username, setUsername] = useState<string>("");
-
-    // useState hook for count
-    const [count, setCount] = useState<number>(0);
-
-    // Handle input change for username
-    function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-        setUsername(event.target.value);
-    }
-
-    // Handle increase button click
-    function increaseCount() {
-        setCount(count + 1);
-    }
-
-    return (
-        <div>
-            <h1>Hello {username}</h1>
-            <p>You can edit this..!</p>
-
-            <label>Enter user name  </label>
-            <input type="text" onChange={handleInputChange} />
-
-            <h1>Count: {count}</h1>
-            <button onClick={increaseCount}>Increase</button>
-
-            <h2>Vehicle</h2>
-            <Vehicle title="Toyota Corolla" description="Toyota Corolla is the highest sold vehicle" />
-            <Vehicle title="Toyota Land Cruiser" description="Toyota Land Cruiser is the highest sold vehicle" />
-        </div>
-    )
+    return Home();
 }
 
 // Define a type
