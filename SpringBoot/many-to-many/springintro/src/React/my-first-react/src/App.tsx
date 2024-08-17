09235f2439f668698a7a32738e99c96f3bb35ce0
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-17 08:56:50 +0530

    move vehiclecomponent and export it

--- SpringBoot/many-to-many/springintro/src/React/my-first-react/src/App.tsx
+++ SpringBoot/many-to-many/springintro/src/React/my-first-react/src/App.tsx
@@ -26,13 +26,13 @@ function App() {
 
 
 // child components
-function Vehicle(props: VehicleType) {
-    return (
-        <div>
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
-        </div>
-    )
-}
+// function Vehicle(props: VehicleType) {
+//     return (
+//         <div>
+//             <h3>{props.title}</h3>
+//             <p>{props.description}</p>
+//         </div>
+//     )
+// }
 
 export default App
