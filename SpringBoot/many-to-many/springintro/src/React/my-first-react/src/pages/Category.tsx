db4785851719cab553f3ffcf49913254d75229df
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-17 10:42:17 +0530

    send first api request via axios

--- SpringBoot/many-to-many/springintro/src/React/my-first-react/src/pages/Category.tsx
+++ SpringBoot/many-to-many/springintro/src/React/my-first-react/src/pages/Category.tsx
@@ -1,7 +1,15 @@
+import axios from "axios";
+
 function Category(){
+
+    function loadCategories(){
+        axios.get("http://localhost:8080/categories")
+    }
+
     return(
         <div>
             <h1>Categories</h1>
+            <button onClick={loadCategories}>load</button>
         </div>
     )
 }
