710872367b86e44c0d84b493a234a1cd4c6261c7
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-31 13:36:01 +0530

    load all exist products

--- SpringBoot/many-to-many/springintro/src/React/my-first-react/src/pages/orders/CreateOrder.tsx
+++ SpringBoot/many-to-many/springintro/src/React/my-first-react/src/pages/orders/CreateOrder.tsx
@@ -6,13 +6,13 @@ function CreateOrder() {
 
     const [products, setProducts] = useState<ProductType[]>([]);
 
-    async function loadProduct(){
+    async function loadProduct() {
 
-        try{
+        try {
 
             const response = await axios.get("http://localhost:8080/products");
             setProducts(response.data)
-        }catch (error){
+        } catch (error) {
             console.log(error)
         }
     }
@@ -26,11 +26,24 @@ function CreateOrder() {
         <div>
             <div className="flex">
                 <div className="w-[400px] border-r border-slate-100 p-2">
-                    <span className="text-xl font-semibold text-white">Products</span>
+                    <span className="text-xl font-semibold text-white block  p-2">Products</span>
+
+                    <div className="mt-2">
+                        {products.map(function (products) {
+                            return (
+                                <div className="border border-slate-100 rounded-lg p-2 mb-3">
+                                    <div className="text-lg font-semibold text-white">{products.name}</div>
+                                    <div className="text-sm text-white">{products.category?.name}</div>
+                                    <div className="text-sm text-green-500 text-right">{products.price}</div>
+                                </div>
+                            )
+                        })}
+                    </div>
+
                 </div>
 
                 <div className="p-2">
-                    <span className="text-xl font-semibold text-white">New Order</span>
+                    <span className="text-xl font-semibold text-white block  p-2">New Order</span>
                 </div>
             </div>
         </div>
