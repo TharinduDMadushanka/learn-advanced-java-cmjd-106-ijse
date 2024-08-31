364d62d163fd69a2c678a64e7c8b438e9f006852
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-31 12:52:15 +0530

    add loadOrders function and useEffect for the when the page load automatically load the orders

--- SpringBoot/many-to-many/springintro/src/React/my-first-react/src/pages/orders/Orders.tsx
+++ SpringBoot/many-to-many/springintro/src/React/my-first-react/src/pages/orders/Orders.tsx
@@ -1,14 +1,36 @@
+import {useEffect, useState} from "react";
+import OrderType from "../../types/OrderType.tsx";
+import axios from "axios";
 
-function Orders(){
+function Orders() {
 
-    return(
+    const [orders, setOrders] = useState<OrderType[]>([]);
+
+    async function loadOrders() {
+
+        try {
+
+            const response = await axios.get("http://localhost:8080/orders")
+            setOrders(response.data);
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    useEffect(function (){
+        loadOrders();
+    })
+
+    return (
 
         <div className="container mx-auto pt-5 pb-5">
             <h1 className="text-3xl font-semibold mb-5">Orders</h1>
 
             <table className="w-full border-separate border-spacing-0 border-none text-left">
 
-                <thead >
+                <thead>
                 <tr>
                     <th className="w-[80px]">Order ID</th>
                     <th className="w-[200px]">Order Date and Time</th>
--- SpringBoot/many-to-many/springintro/src/main/java/com/ijse/springintro/controller/OrderController.java
+++ SpringBoot/many-to-many/springintro/src/main/java/com/ijse/springintro/controller/OrderController.java
@@ -5,10 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ijse.springintro.dto.OrderDTO;
 import com.ijse.springintro.entity.Order;
@@ -17,6 +14,7 @@ import com.ijse.springintro.service.OrderService;
 import com.ijse.springintro.service.ProductService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class OrderController {
 
     @Autowired
