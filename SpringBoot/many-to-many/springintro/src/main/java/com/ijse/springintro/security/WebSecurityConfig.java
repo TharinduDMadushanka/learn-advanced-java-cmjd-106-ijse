e478b522cddbd9acf7a18ddd69b3739d4498378c
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-07 12:27:53 +0530

    complete Authentication security configurations

--- SpringBoot/many-to-many/springintro/src/main/java/com/ijse/springintro/security/WebSecurityConfig.java
+++ SpringBoot/many-to-many/springintro/src/main/java/com/ijse/springintro/security/WebSecurityConfig.java
@@ -6,8 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.security.Security;
 
 @Configuration
 @EnableMethodSecurity
@@ -26,6 +32,21 @@ public class WebSecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/auth/****").permitAll()
+                                .anyRequest().authenticated()
+                );
+
+        //add filter
+        http.addFilterBefore(authenticationJwtTokenFilter(), AuthTokenFilter.class);
+        return http.build();
     }
 
 }
