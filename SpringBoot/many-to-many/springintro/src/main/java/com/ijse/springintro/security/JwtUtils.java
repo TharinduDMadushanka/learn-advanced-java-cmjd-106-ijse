107b65f29cf24480f4470e1728de91de5cbd7038
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-07 10:18:26 +0530

    add jwt dependencies

--- SpringBoot/many-to-many/springintro/src/main/java/com/ijse/springintro/security/JwtUtils.java
+++ SpringBoot/many-to-many/springintro/src/main/java/com/ijse/springintro/security/JwtUtils.java
@@ -1,7 +1,18 @@
 package com.ijse.springintro.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+
+import java.security.Key;
 
 @Configuration
 public class JwtUtils {
+
+    @Value("${app.secret}") // get secret key from application.properties
+    private String secret;
+
+    private Key key(){
+
+    }
+
 }
