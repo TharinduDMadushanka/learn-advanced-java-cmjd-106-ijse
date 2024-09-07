5e9df538c0170df6bbff2934e83c826fc70c2e13
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-07 11:50:04 +0530

    complete filter token method in AuthTokenFilter class

---
+++ SpringBoot/many-to-many/springintro/src/main/java/com/ijse/springintro/security/AuthTokenFilter.java
@@ -0,0 +1,36 @@
+package com.ijse.springintro.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    //filter token
+    private String parseJwtFromHeader(HttpServletRequest request) {
+
+        String authHeader = request.getHeader("Authorization");
+        if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7); //remove first 7 characters from token which is "Bearer" then now have token only
+        }else {
+            return null;
+        }
+
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // check JWT token in the request
+
+    }
+}
