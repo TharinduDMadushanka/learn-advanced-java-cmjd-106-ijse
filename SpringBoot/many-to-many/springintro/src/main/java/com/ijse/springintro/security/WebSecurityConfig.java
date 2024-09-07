0a74cf86f5be656b15b5336d29d1c262a3ab8fdf
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-07 12:21:12 +0530

    complete Authentication fuilter bean register

---
+++ SpringBoot/many-to-many/springintro/src/main/java/com/ijse/springintro/security/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.ijse.springintro.security;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableMethodSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+}
