19b969a1f89b055acb2c9be3b0d56c0d3758df13
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-14 10:12:03 +0530

    created function to submit and set onchange functions int o field

--- SpringBoot/many-to-many/springintro/src/React/my-first-react/src/pages/Auth/Login.tsx
+++ SpringBoot/many-to-many/springintro/src/React/my-first-react/src/pages/Auth/Login.tsx
@@ -1,6 +1,15 @@
-function Login(){
+import {useState} from "react";
 
-    return(
+function Login() {
+
+    const [username, setUsername] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+
+    function submit() {
+
+    }
+
+    return (
         <div className="p-10 border border-gray-50 rounded-lg">
 
             <div className="max-w-[600px] p-5 shadow rounded-lg mx-auto">
@@ -11,16 +20,22 @@ function Login(){
                 <form>
                     <div className="mb-4 mt-8">
                         <label className="block mb-1 text-start">Username</label>
-                        <input type="text" className="block w-full p-2 border border-gray-50 rounded-lg "
+                        <input type="text" onChange={function (event) {
+                            setUsername(event.target.value);
+                        }}
+                               className="block w-full p-2 border border-gray-50 rounded-lg "
                                placeholder="Username"/>
                     </div>
                     <div className="mb-4">
                         <label className="block mb-1 text-start">Password</label>
-                        <input type="password" className="block w-full p-2 border border-gray-50 rounded-lg"
+                        <input type="password" onChange={function (event) {
+                            setPassword(event.target.value)
+                        }}
+                               className="block w-full p-2 border border-gray-50 rounded-lg"
                                placeholder="Password"/>
                     </div>
 
-                    <div >
+                    <div>
                         <button type="submit" className="bg-sky-400 text-black px-4 py-2 rounded-lg
                         w-full hover:bg-sky-600 mt-10">
                             Login
