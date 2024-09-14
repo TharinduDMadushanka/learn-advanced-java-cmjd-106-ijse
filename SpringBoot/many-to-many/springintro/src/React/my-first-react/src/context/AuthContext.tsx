09e77e858755ad27cc3f3230d3d57d872153df31
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-14 11:16:35 +0530

    bug fix in AuthContext.tsx

--- SpringBoot/many-to-many/springintro/src/React/my-first-react/src/context/AuthContext.tsx
+++ SpringBoot/many-to-many/springintro/src/React/my-first-react/src/context/AuthContext.tsx
@@ -3,33 +3,35 @@ import AuthContextType from "../types/AuthContextType.tsx";
 import AuthProviderPropsType from "../types/AuthProviderPropsType.tsx";
 
 export const AuthContext = createContext<AuthContextType>({
-   isAuthenticated: false,
-   jwtToken: null,
-   login: () => {},
-   logout: () =>{}
+    isAuthenticated: false,
+    jwtToken: null,
+    login: () => {
+    },
+    logout: () => {
+    }
 });
 
-export function AuthProvider({children}: AuthProviderPropsType){
+export function AuthProvider({children}: AuthProviderPropsType) {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
     const [jwtToken, setJwtToken] = useState<string | null>(null);
 
-    function login(jwtToken: string){
+    function login(jwtToken: string) {
         setIsAuthenticated(true);
         setJwtToken(jwtToken);
     }
 
-    function logout(){
+    function logout() {
         setIsAuthenticated(false);
         setJwtToken(null)
     }
 
-    return(
-        <AuthContext.Provider value={{ isAuthenticated, jwtToken, login, logout}}>
+    return (
+        <AuthContext.Provider value={{isAuthenticated, jwtToken, login, logout}}>
             {children}
         </AuthContext.Provider>
     )
 }
 
 export function useAuth() {
-    useContext(AuthContext)
+    return useContext(AuthContext)
 }
