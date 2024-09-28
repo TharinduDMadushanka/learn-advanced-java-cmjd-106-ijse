0fe3efc36e392400c5de29c8d149fd2bc3f633e5
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-28 10:17:37 +0530

    call connectDB();

--- ExpressJS/src/app.ts
+++ ExpressJS/src/app.ts
@@ -1,11 +1,14 @@
 import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db';
 
 dotenv.config();
 
 const app: Application = express();
 
 app.use(express.json());
+
+connectDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, world!");  // Send a response
