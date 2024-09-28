0107b84d4568ae656acc3b7b4a26c150f5dcda69
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-09-28 10:34:25 +0530

    created CategoryModel

---
+++ ExpressJS/src/models/CategoryModel.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+import CategoryType from "../inerfaces/CategoryType";
+
+const categorySchema = new Schema<CategoryType>({
+    name: {
+        type: String,
+        required: true,
+        trim: true // trim whitespaces at the end
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now 
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now 
+    }
+});
+
+const CategoryModel = mongoose.model<CategoryType>('Catgory', categorySchema)
+
+export default CategoryModel; // similar to entity in Springboot
